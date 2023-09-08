49ba855cb9db635435fbc3cb1d410de01516684c
Victor Caruso <vic.caruso@gmail.com>
2023-09-08 12:40:59 -0700

    Add global state via context to control sound

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,21 @@
+'use client';
 import React from 'react';
 
 import Header from '../components/Header';
 
 import './styles.css';
+export const SoundEnabledContext = React.createContext();
 
 function RootLayout({ children }) {
+  const [isSoundEnabled, setIsSoundEnabled] = React.useState(true);
+  const value = { isSoundEnabled, setIsSoundEnabled };
   return (
     <html lang="en">
       <body>
-        <Header />
-        {children}
+        <SoundEnabledContext.Provider value={value}>
+          <Header />
+          {children}
+        </SoundEnabledContext.Provider>
         <footer>
           <img src="/ie-badge.gif" width={100} />
           <span>Thanks for visiting!</span>
